5757f69262fe286435ab0aeb808aaad008301684
danvega <danvega@gmail.com>
2025-04-16 21:47:17 -0400

    Adding additional presentations

--- src/main/java/dev/danvega/javaone/PresentationTools.java
+++ src/main/java/dev/danvega/javaone/PresentationTools.java
@@ -13,7 +13,12 @@ public class PresentationTools {
     public PresentationTools() {
         var keynoteOne = new Presentation("Java 24 Launch - Live from JavaOne 2025", "https://www.youtube.com/watch?v=mk_2MIWxLI0", 2025);
         var keynoteTwo = new Presentation("Java Turns 30 - Live from JavaOne 2025", "https://www.youtube.com/watch?v=GwR7Gvi80Xo", 2025);
-        this.presentations.addAll(List.of(keynoteOne,keynoteTwo));
+        var concerto = new Presentation("Concerto for Java & AI - Building Production-Ready LLM Applications","https://www.youtube.com/watch?v=MhILqEAscss", 2025);
+        var gatherers = new Presentation("Stream Gatherers - Deep Dive with the Expert","https://www.youtube.com/watch?v=v_5SKpfkI2U&t=6s", 2025);
+        var ai202 = new Presentation("AI 202: Next-Level AI Mastery for Java Developers","https://www.youtube.com/watch?v=DIyj_V8h8X0",2025);
+        var sequenced = new Presentation("Sequenced Collections - Deep Dive with the Expert","https://www.youtube.com/watch?v=6yuDqkkYTGU",2025);
+
+        this.presentations.addAll(List.of(keynoteOne,keynoteTwo,concerto,gatherers,ai202,sequenced));
     }
 
     public List<Presentation> getPresentations() {
